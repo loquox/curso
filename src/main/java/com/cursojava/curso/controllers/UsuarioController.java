3649d6d805e7fe19605c1ba08f8f6b1f3d43fc9c
loquox <crloquox@gmail.com>
2022-11-01 21:06:57 +0100

    primer Spring

---
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -0,0 +1,58 @@
+package com.cursojava.curso.controllers;
+
+import com.cursojava.curso.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsuarioController {
+    @RequestMapping(value = "usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long  id ) {
+        Usuario usuario= new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("LucasMoy@hotmail.com");
+        usuario.setTelefono("343434");
+        return usuario;
+
+        }
+
+    @RequestMapping(value = "usuario")
+    public Usuario editar() {
+        Usuario usuario= new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("LucasMoy@hotmail.com");
+        usuario.setTelefono("343434");
+        return usuario;
+
+    }
+
+    @RequestMapping(value = "usuario")
+    public Usuario eliminar() {
+        Usuario usuario= new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("LucasMoy@hotmail.com");
+        usuario.setTelefono("343434");
+        return usuario;
+
+    }
+
+    @RequestMapping(value = "usuario")
+    public Usuario buscar() {
+        Usuario usuario= new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellido("Moy");
+        usuario.setEmail("LucasMoy@hotmail.com");
+        usuario.setTelefono("343434");
+        return usuario;
+
+    }
+
+
+
+
+}
